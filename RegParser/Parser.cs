a15bed0891446ef2f375a5c27b0e2288c57a93c0
whykhamist <jhydenellana@gmail.com>
2021-04-21 13:15:49 +0800

    Created the Registry Object

---
+++ RegParser/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegParser
+{
+    /// <summary>
+    /// This Class helps in creating a registry backup based on provided registry key
+    /// as well as retore them using only the built in Registry class of c#,
+    /// with that said, this only supports specific Registry data types supported by the said class.
+    /// 
+    /// Supported types { REG_SZ, REG_DWORD, REG_QWORD, REG_BINARY, REG_EXPAND_SZ, REG_MULTI_SZ REG_NONE }
+    /// 
+    /// Unsupported types will likely be treated as string. Any improvement to this class are welcome! (^_^)
+    /// </summary>
+    public static class Parser
+    {
+    }
+}
---
+++ RegParser/RegistryObject.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+
+namespace RegParser
+{
+    public class RegistryObject
+    {
+        public RegistryValueKind ValueKind { get; set; }
+        public string KeyName { get; set; }
+        public object Value { get; set; }
+
+        public RegistryObject() { }
+
+        public RegistryObject(string keyName, object value, RegistryValueKind valueKind)
+        {
+            KeyName = keyName;
+            Value = value;
+            ValueKind = valueKind;
+        }
+    }
+}
